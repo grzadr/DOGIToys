572a684300d36b10c7829e9f2c547cf022a0ae61
Adrian Grzemski <adrian.grzemski@gmail.com>
2018-11-13 14:52:07 +0100

    working on populating GenomicSequences

--- src/initiate.cpp
+++ src/initiate.cpp
@@ -7,13 +7,14 @@ void DOGIToys::Initiate::init_main(QSqlDatabase db) {
 
 void DOGIToys::Initiate::init_taxon(QSqlDatabase db, QString taxons) {
   qInfo() << "Initiating Taxons";
+  Execute::exec(db, Schemas::Taxons);
   if (taxons.isEmpty()) {
-    db.transaction();
+    Transaction::transaction(db);
     auto insert = prepare(db,
-                          "INSERT INTO DOGITaxons (id_taxon, taxon_name) "
+                          "INSERT INTO Taxons (id_taxon, taxon_name) "
                           "VALUES (:id_taxon, :taxon_name)");
 
-    for (const auto& [id_taxon, name] : Schemas::Taxons) {
+    for (const auto& [id_taxon, name] : Schemas::BasicTaxonIDs) {
       insert.bindValue(":id_taxon", id_taxon);
       insert.bindValue(":taxon_name", name);
       exec(insert);
@@ -21,17 +22,18 @@ void DOGIToys::Initiate::init_taxon(QSqlDatabase db, QString taxons) {
     }
 
     prepare(insert,
-            "INSERT INTO DOGITaxonAliases (id_alias, id_taxon) "
+            "INSERT INTO TaxonAliases (id_alias, id_taxon) "
             "VALUES (:id_alias, :id_taxon)");
 
-    for (const auto& [id_alias, id_taxon] : Schemas::TaxonAliases) {
+    for (const auto& [id_alias, id_taxon] : Schemas::BasicTaxonAliases) {
       insert.bindValue(":id_alias", id_alias);
       insert.bindValue(":id_taxon", id_taxon);
       exec(insert);
       insert.finish();
     }
 
-    db.commit();
+    Transaction::commit(db);
+
   } else {
     throw_runerror("Loading taxons from external file is not supported!!!");
   }
@@ -41,4 +43,9 @@ void DOGIToys::Initiate::init_genomic_features(QSqlDatabase db) {
   qInfo() << "Initiating GenomicFeatures";
   Execute::exec(db, Schemas::SeqIDs);
   Execute::exec(db, Schemas::GenomicFeatures);
+}
+
+void DOGIToys::Initiate::init_genomic_sequences(QSqlDatabase db) {
+  qInfo() << "Initiating GenomicSequences";
+  Execute::exec(db, Schemas::GenomicSequences);
 }
